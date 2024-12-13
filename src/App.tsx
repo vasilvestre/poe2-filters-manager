3376a46006a39d5bb775edebacfe9e90f8c82aa6
Valentin Silvestre <vasilvestre@laposte.net>
2024-12-13 13:59:00 +0100

    build: Add some CI and quality tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,128 +1,148 @@
-import "./App.css";
-import {fetch} from '@tauri-apps/plugin-http'
-import {invoke} from "@tauri-apps/api/core";
-import {path} from "@tauri-apps/api";
-import {download} from "@tauri-apps/plugin-upload";
-import {Octokit} from 'octokit'
-import {Endpoints} from '@octokit/types'
-import { exists, mkdir, readDir, remove, rename} from "@tauri-apps/plugin-fs";
-import {resolve} from "@tauri-apps/api/path";
-import {useState} from "react";
+import './App.css'
+import { Endpoints } from '@octokit/types'
+import { path } from '@tauri-apps/api'
+import { invoke } from '@tauri-apps/api/core'
+import { resolve } from '@tauri-apps/api/path'
+import { exists, mkdir, readDir, remove, rename } from '@tauri-apps/plugin-fs'
+import { fetch } from '@tauri-apps/plugin-http'
+import { download } from '@tauri-apps/plugin-upload'
+import { Octokit } from 'octokit'
+import { useState } from 'react'
 
-export type GithubRelease = Endpoints['GET /repos/{owner}/{repo}/releases/latest']['response']
+export type GithubRelease =
+  Endpoints['GET /repos/{owner}/{repo}/releases/latest']['response']
 
 function App() {
-    const [downloading, setDownloading] = useState(false);
+  const [downloading, setDownloading] = useState(false)
 
-    async function downloadZipAssetFromLatestRelease(repoOwner: string, repoName: string) {
-        const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/releases/latest`);
-        if (!response.ok) {
-            throw new Error(`Error fetching release information: ${response.status} ${response.statusText}`);
+  async function downloadZipAssetFromLatestRelease(repoOwner: string, repoName: string) {
+    const response = await fetch(
+      `https://api.github.com/repos/${repoOwner}/${repoName}/releases/latest`
+    )
+    if (!response.ok) {
+      throw new Error(
+        `Error fetching release information: ${response.status} ${response.statusText}`
+      )
+    }
+
+    const octokit = new Octokit({
+      appId: import.meta.env.GITHUB_APP_ID as string,
+      privateKey: import.meta.env.GITHUB_PRIVATE_KEY as string,
+      clientId: import.meta.env.GITHUB_CLIENT_ID as string,
+      clientSecret: import.meta.env.GITHUB_CLIENT_SECRET as string
+    })
+
+    const release: GithubRelease = await octokit.request(
+      'GET /repos/{owner}/{repo}/releases/latest',
+      {
+        owner: repoOwner,
+        repo: repoName,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
         }
+      }
+    )
 
-        const octokit = new Octokit({
-            appId: import.meta.env.GITHUB_APP_ID,
-            privateKey: import.meta.env.GITHUB_PRIVATE_KEY,
-            clientId: import.meta.env.GITHUB_CLIENT_ID,
-            clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
-        })
+    const releaseData = release.data
 
-        const release: GithubRelease = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
-            owner: repoOwner,
-            repo: repoName,
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28',
-            },
-        })
+    const zipballUrl = releaseData.zipball_url
 
-        const releaseData = release.data
+    if (!zipballUrl) {
+      throw new Error('Error fetching zipball url')
+    }
 
-        const zipballUrl = releaseData.zipball_url
+    const zipTarget = await path.resolve(
+      await path.downloadDir(),
+      `${repoOwner}-${repoName}-${releaseData.tag_name}.zip`
+    )
 
-        if (!zipballUrl) {
-            throw new Error('Error fetching zipball url')
-        }
+    const downloadDir = await resolve(
+      await path.downloadDir(),
+      `${repoOwner}-${repoName}`
+    )
 
-        const zipTarget = await path.resolve(
-            await path.downloadDir(),
-            `${repoOwner}-${repoName}-${releaseData.tag_name}.zip`
+    if (!(await exists(downloadDir))) {
+      setDownloading(true)
+      await download(
+        zipballUrl,
+        zipTarget,
+        progress => console.log(`Downloading ${progress.progress}`), // a callback that will be called with the download progress
+        new Map([['user-agent', 'cors-bypass']])
+      )
+      setDownloading(false)
+      await mkdir(downloadDir)
+
+      await invoke('extract', { srcZip: zipTarget, outDir: downloadDir })
+    }
+
+    if (await exists(zipTarget)) {
+      await remove(zipTarget)
+    }
+
+    const entries = await readDir(downloadDir)
+
+    for (const file of entries) {
+      if (file.name.endsWith('.filter')) {
+        await rename(
+          await resolve(downloadDir, file.name),
+          await resolve(
+            await path.documentDir(),
+            `My Games\\Path of Exile 2\\${file.name}`
+          )
         )
-
-        const downloadDir = await resolve(await path.downloadDir(), `${repoOwner}-${repoName}`)
-
-        if (!await exists(downloadDir)) {
-            setDownloading(true)
-            await download(
-                zipballUrl,
-                zipTarget,
-                (progress) => console.log(`Downloading ${progress.progress}`), // a callback that will be called with the download progress
-                new Map([['user-agent', 'cors-bypass']])
-            )
-            setDownloading(false)
-            await mkdir(downloadDir)
-
-            await invoke('extract', {srcZip: zipTarget, outDir: downloadDir})
-        }
-
-        if (await exists(zipTarget)) {
-            await remove(zipTarget)
-        }
-
-        const entries = await readDir(downloadDir);
-
-        for (const file of entries) {
-            file.name.endsWith('.filter')
-                ? await rename(await resolve(downloadDir, file.name), await resolve(await path.documentDir(), `My Games\\Path of Exile 2\\${file.name}`))
-                : await remove(await resolve(downloadDir, file.name));
-        }
-
-        await remove(downloadDir)
+      } else {
+        await remove(await resolve(downloadDir, file.name))
+      }
     }
 
-    async function downloadLatestFilter() {
-        downloadZipAssetFromLatestRelease('NeverSinkDev', 'NeverSink-PoE2litefilter');
-    }
+    await remove(downloadDir)
+  }
 
-    return (
-        <main>
-            <div className="navbar bg-base-100">
-                <a className="btn btn-ghost text-xl">
-                    POE2 Filters Manager
-                </a>
-                {downloading &&
-                    <span className="loading loading-bars loading-md"></span>
-                }
-            </div>
-            <section>
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        {/* head */}
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Repository</th>
-                            <th>Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr className="hover">
-                            <td>NeverSink</td>
-                            <td>https://github.com/NeverSinkDev/NeverSink-PoE2litefilter</td>
-                            <td>
-                                <button className="btn btn-ghost text-md" onClick={(e) => {
-                                    e.preventDefault();
-                                    downloadLatestFilter();
-                                }}>
-                                    Télécharger le filtre
-                                </button>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </section>
-        </main>
-    );
+  async function downloadLatestFilter() {
+    await downloadZipAssetFromLatestRelease('NeverSinkDev', 'NeverSink-PoE2litefilter')
+  }
+
+  return (
+    <main>
+      <div className="navbar bg-base-100">
+        <a className="btn btn-ghost text-xl">POE2 Filters Manager</a>
+        {downloading && <span className="loading loading-bars loading-md"></span>}
+      </div>
+      <section>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Repository</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="hover">
+                <td>NeverSink</td>
+                <td>https://github.com/NeverSinkDev/NeverSink-PoE2litefilter</td>
+                <td>
+                  <button
+                    className="btn btn-ghost text-md"
+                    onClick={e => {
+                      e.preventDefault()
+                      downloadLatestFilter()
+                        .then(r => console.log(r))
+                        .catch(e => console.error(e))
+                    }}
+                  >
+                    Télécharger le filtre
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImprotMetaEnv {
+  readonly VITE_GITHUB_APP_ID: string
+  readonly VITE_GITHUB_CLIENT_ID: string
+  readonly VITE_GITHUB_CLIENT_SECRET: string
+  readonly VITE_GITHUB_PRIVATE_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
